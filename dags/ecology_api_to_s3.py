071d06b3848aaa82766a6b5c51e5b9e3c4029f1f
WorkIvanAccount <45nt27v64n@privaterelay.appleid.com>
2026-08-22 12:01:57 +0300

    Изменено req.txt, пересоздан duckdb pendulum: - Добавлены зависимости - Переименован raw_from_api_to_s3.py

---
+++ dags/ecology_api_to_s3.py
@@ -0,0 +1,7 @@
+import logging
+import duckdb
+import pendulum
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
